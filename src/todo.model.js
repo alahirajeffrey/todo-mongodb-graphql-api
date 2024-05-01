a982548cfa375216933e5564dd3bcf11d4ccb46a
alahirajeffrey <alahirajeffrey@gmail.com>
2024-05-01 01:34:17 +0100

    removed field

--- src/todo.model.js
+++ src/todo.model.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
-  // id: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   isCompleted: { type: Boolean, default: false },
